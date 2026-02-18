9a02e04d3fd9dcf37fdb970dfcaf07a52460d3db
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-02-18 16:22:28 +0100

    chore: added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,13 +26,19 @@ repositories {
 extra["otelVersion"] = "2.21.0"
 extra["springCloudVersion"] = "2025.0.0"
 extra["testcontainersVersion"] = "1.19.8"
+extra["testKeycloakVersion"] = "3.9.0"
 
 val otelVersion: String by project
 val springCloudVersion: String by project
+val testcontainersVersion: String by project
+val testKeycloakVersion: String by project
 
 dependencies {
 	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	// Spring Cloud
@@ -44,19 +50,31 @@ dependencies {
 	// Runtime
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 	runtimeOnly("io.opentelemetry.javaagent:opentelemetry-javaagent:$otelVersion")
+	runtimeOnly("org.flywaydb:flyway-core")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:r2dbc-postgresql")
+	runtimeOnly("org.springframework:spring-jdbc")
 
 	// MacOS Apple Silicon
 	// runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.101.Final:osx-aarch_64")
 
 	// Test
+	testImplementation("io.r2dbc:r2dbc-h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.springframework.cloud:spring-cloud-stream")
+	testImplementation("com.squareup.okhttp3:mockwebserver")
+	testImplementation("com.github.dasniko:testcontainers-keycloak:$testKeycloakVersion")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:r2dbc")
 	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+	testImplementation("org.springframework.cloud:spring-cloud-stream")
 }
 
 dependencyManagement {
 	imports {
+		mavenBom("org.testcontainers:testcontainers-bom:$testcontainersVersion")
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
 	}
 }
